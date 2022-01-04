17bd3aae9db4293e41ac516d487ba9dfe68ea0a2
Ruiqi Pan <rikki@haneikis-MacBook-Pro.local>
2022-01-04 15:19:49 -0600

    filter by time conflicts

--- src/App.js
+++ src/App.js
@@ -48,24 +48,108 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-// make a bootstrap card
-const Course = ({ course }) => (
-  <div className="card m-2 p-2">
-    <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-      <div className="card-text">{ course.title }</div>
-    </div>
-  </div>
+
+// select a course: new list of selected courses that includes the course
+// unselect a course: create a new list of selected courses that omits the course
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
 
+// make a bootstrap card
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const isDisabled = !isSelected && hasConflict(course, selected);
+  const style = {
+    backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
+  };
+  return (
+    <div className="card m-1 p-2" 
+      style={style}
+      onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
+      <div className="card-body">
+        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+        <div className="card-text">{ course.title }</div>
+      </div>
+    </div>
+  );
+};
+
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+);
+
+// parse meeting strings for each course into structured form
+// to check if courses are in the same term and overlap
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+
+// add new fields to each course when the courses are first fetched
+const mapValues = (fn, obj) => (
+  // use Object.fromEntries to create a new object from an old one
+  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
+
+
+// define conlict
+// if course conflict, disable the unselected course
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+
+// Courselist
 const CourseList = ({ courses }) => {
+  // filter by term
   const [term, setTerm] = useState('Fall');
+  // select course and filter by time conflicts
+  const [selected, setSelected] = useState([]);
+
   const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
   return (
     <>
       <TermSelector term={term} setTerm={setTerm}/>
       <div className="course-list">
-        { termCourses.map(course => <Course key={course.id} course={ course } />) }
+        { termCourses.map(course =>
+          <Course key={ course.id } course={ course }
+            selected={selected} setSelected={ setSelected } 
+          />)  
+         }
       </div>
     </>
   );
@@ -114,7 +198,7 @@ const App = () =>  {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }, []);
