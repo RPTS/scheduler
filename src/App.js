f5f8285fc487c2a5b9412e8d99ddde2d17075a46
Ruiqi Pan <rikki@haneikis-MacBook-Pro.local>
2022-01-04 14:31:28 -0600

    Filter by term

--- src/App.js
+++ src/App.js
@@ -58,16 +58,45 @@ const Course = ({ course }) => (
   </div>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-    { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm}/>
+      <div className="course-list">
+        { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      </div>
+    </>
+  );
+};
+
+// implement a radio button
+const TermButton = ({term,setTerm,checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" autoComplete="off" checked={checked} onChange={() => setTerm(term)}/>
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+// btn-group can be used to make a row of bottons
+const TermSelector = ({term,setTerm}) => (
+  <div className="btn-group">
+  { 
+    Object.values(terms).map(value => (
+      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+    ))
+  }
   </div>
 );
+
+
 
 // <Banner title={ schedule.title } /> 
 // collects the attributes into a props objects, and passes that object to the function
 // so React calls function Banner with object {"title": "CS Courses..."}
-
 const App = () =>  {
 
   // we used array destructuring to assign the array value to schedule, setSchedule
